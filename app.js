0b57681740150a1c5732e930b06a61157f98ddc6
Simonebaca <simobaco99@gmail.com>
2023-03-29 18:16:48 +0200

    reset vue to fix

--- app.js
+++ app.js
@@ -5,34 +5,53 @@ Bonus:
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce*/
 
-const { createApp } = Vue
-  createApp({
-    data() {
-      return {
-        activeImage: 5,
-        movie: {
-          titles: [
-            "Marvel's Spiderman Miles Morale",
-            "Ratchet & Clank: Rift Apart",
-            "Fortnite",
-            "Stray",
-            "Marvel's Avengers",
-          ],
-          descriptions: [
-            "lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor"
-          ],
-          images: [
-            "img/01.webp",
-            "img/02.webp",
-            "img/03.webp",
-            "img/04.webp",
-            "img/05.webp",
-          ],
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      tabindex: 0,
+      slides: [
+        {
+          image: "img/01.webp",
+          title: "Marvel's Spiderman Miles Morale",
+          text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+        },
+        {
+          image: "img/02.webp",
+          title: "Ratchet & Clank: Rift Apart",
+          text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        },
+        {
+          image: "img/03.webp",
+          title: "Fortnite",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        },
+        {
+          image: "img/04.webp",
+          title: "Stray",
+          text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        },
+        {
+          image: "img/05.webp",
+          title: "Marvel's Avengers",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        },
+      ],
+    };
+  },
+  methods:{
+    prevImg(){
+        this.tabindex--;
+        if(this.tabindex < 0){
+            this.tabindex = this.slides.length - 1;
         }
-      }
     },
-    mounted() {
-      const myTimer = setInterval(() => {
-      }, 3000);
+    nextImg(){
+        this.tabindex++;
+        if(this.tabindex > this.slides.length - 1){
+            this.tabindex = 0;
+        }
     }
-  }).mount('#app')
+  }
+}).mount("#app");
